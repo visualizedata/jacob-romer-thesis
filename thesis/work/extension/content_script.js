76ffacf07fbf6dce982fd601122d896f64e37966
Jacob Romer <jmbrjmbr@gmail.com>
2016-04-06 11:56:31 -0400

    sort H1s by boundingClientRect area

--- thesis/work/extension/content_script.js
+++ thesis/work/extension/content_script.js
@@ -1,11 +1,17 @@
 window.setTimeout(() => {
-  [].slice.call(document.getElementsByTagName("h1")).map((el,i) => {
+  [].slice.call(document.getElementsByTagName("h1")).sort((pre, cur) => {
+    return (
+      (cur.getBoundingClientRect().width * cur.getBoundingClientRect().height) -
+      (pre.getBoundingClientRect().width * pre.getBoundingClientRect().height) 
+    )
+  }).map((el, i) => {
     if(i === 0) {
+      console.log(el)
       el.classList.add("bounce", "animated", "textmarker", "invers")
       window.setTimeout(() => {
-        $(el).typed({strings: ["Hello.", "I want to play a game", "This is a demo of what you can do with a sime content script"], typeSpeed: 10, startDelay: 200});
-      }, 700);
+        $(el).typed({strings: ["Hello.", "Content Scripts are great.", `Christ on a cross, this site has ${Math.floor(Math.random() * 40 + 1)} trackers!!!1!`], typeSpeed: 10, startDelay: 200});
+      }, 1700);
     }
   });
-}, 1000);
+}, 2000);
 
